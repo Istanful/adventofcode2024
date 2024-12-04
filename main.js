5d910bf224416e586edca119540d1c307af652b5
Emric <emric.mansson@gmail.com>
2024-12-04 15:42:09 +0100

    Fix README generator

--- main.js
+++ main.js
@@ -2,46 +2,51 @@ const days = require("./daysConfig");
 const Runner = require("./Runner");
 const fs = require("node:fs");
 
-let output = "";
-output += "# Advent Of Code 2024\n";
-output += "\n";
-output += "## Completed\n";
-output += "\n";
-output += `| Day | Part 1 | Part 2 |\n`;
-output += `| --- | ------ | ------ |\n`;
+async function run() {
+  let output = "";
+  output += "# Advent Of Code 2024\n";
+  output += "\n";
+  output += "## Completed\n";
+  output += "\n";
+  output += `| Day | Part 1 | Part 2 |\n`;
+  output += `| --- | ------ | ------ |\n`;
 
-days.forEach(async (day, i) => {
-  const dayNumber = i + 1;
-  const dayName = `day${dayNumber.toString().padStart(2, "0")}`;
-  console.log(`Solving ${dayName}...`);
-  let runner = null;
+  for (let i = 0; i < days.length; i++) {
+    const day = days[i];
+    const dayNumber = i + 1;
+    const dayName = `day${dayNumber.toString().padStart(2, "0")}`;
+    console.log(`Solving ${dayName}...`);
+    let runner = null;
 
-  try {
-    runner = require(`./${dayName}/index.js`);
-  } catch (e) {
-    runner = new Runner.Node();
+    try {
+      runner = require(`./${dayName}/index.js`);
+    } catch (e) {
+      runner = new Runner.Node();
+    }
+
+    const solutions = await runner.solve({
+      ...day,
+      name: dayName,
+      number: dayNumber,
+    });
+    output += `| ${dayNumber} `;
+
+    if (solutions.part1.answer === day.part1.answer) {
+      output += `| ★ `;
+    } else {
+      console.log(`Got ${dayNumber} part 1 answer: ${solutions.part1.answer}`);
+      output += `| ☆ `;
+    }
+
+    if (solutions.part2.answer === day.part2.answer) {
+      output += `| ★  |\n`;
+    } else {
+      console.log(`Got ${dayNumber} part 2 answer: ${solutions.part2.answer}`);
+      output += `| ☆  |\n`;
+    }
   }
 
-  const solutions = await runner.solve({
-    ...day,
-    name: dayName,
-    number: dayNumber,
-  });
-  output += `| ${dayNumber} `;
+  fs.writeFileSync(`./README.md`, output, "utf8");
+}
 
-  if (solutions.part1.answer === day.part1.answer) {
-    output += `| ★ `;
-  } else {
-    console.log(`Got ${dayNumber} part 1 answer: ${solutions.part1.answer}`);
-    output += `| ☆ `;
-  }
-
-  if (solutions.part2.answer === day.part2.answer) {
-    output += `| ★  |\n`;
-  } else {
-    console.log(`Got ${dayNumber} part 2 answer: ${solutions.part2.answer}`);
-    output += `| ☆  |\n`;
-  }
-});
-
-fs.writeFileSync(`./README.md`, output, "utf8");
+run();
