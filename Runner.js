9a6b8913bfef0468e4ea1715f9ef79ae50606576
Emric <emric.mansson@gmail.com>
2024-12-04 19:37:04 +0100

    Change runner structure

--- Runner.js
+++ Runner.js
@@ -4,11 +4,10 @@ const sessionCookie = fs.readFileSync("./sessionCookie.txt", "utf8").trim();
 
 class NodeRunner {
   async solve(dayConfig) {
-    const { name, number } = dayConfig;
+    const { input } = dayConfig;
 
-    const input = await this.getInput(name, number);
-    const part1Answer = this.run(`${__dirname}/${name}/part1.js`, input);
-    const part2Answer = this.run(`${__dirname}/${name}/part2.js`, input);
+    const part1Answer = this.run(this.solutionPath(dayConfig, 1), input);
+    const part2Answer = this.run(this.solutionPath(dayConfig, 2), input);
 
     return {
       part1: {
@@ -20,39 +19,36 @@ class NodeRunner {
     };
   }
 
-  run(path, input) {
-    if (!fs.existsSync(path)) {
-      const template = [
-        "function solve(input) {",
-        "}",
-        "module.exports = solve",
-      ];
-      fs.writeFileSync(path, template);
-    }
+  getTemplates(dayConfig) {
+    const content = [
+      "function solve(input) {",
+      "}",
+      "module.exports = solve",
+    ].join("\n");
 
+    return [
+      {
+        path: this.solutionPath(dayConfig, 1),
+        content,
+      },
+      {
+        path: this.solutionPath(dayConfig, 2),
+        content,
+      },
+    ];
+  }
+
+  solutionPath({ name }, partNo) {
+    return `${__dirname}/${name}/part${partNo}.js`;
+  }
+
+  run(path, input) {
     try {
       const func = require(path);
       return func(input);
     } catch (e) {
       return undefined;
     }
-  }
-
-  async getInput(name, dayNumber) {
-    const inputPath = `${__dirname}/${name}/input.txt`;
-
-    if (!fs.existsSync(inputPath)) {
-      const url = `https://adventofcode.com/2024/day/${dayNumber}/input`;
-      const newInput = await fetch(url, {
-        headers: {
-          cookie: sessionCookie,
-        },
-      }).then((response) => response.text());
-      fs.writeFileSync(inputPath, newInput, "utf8");
-      return newInput;
-    }
-
-    return fs.readFileSync(inputPath, "utf8");
   }
 }
 
--- main.js
+++ main.js
@@ -2,51 +2,107 @@ const days = require("./daysConfig");
 const Runner = require("./Runner");
 const fs = require("node:fs");
 
+const sessionCookie = fs.readFileSync("./sessionCookie.txt", "utf8").trim();
+
 async function run() {
   let output = "";
   output += "# Advent Of Code 2024\n";
   output += "\n";
   output += "## Completed\n";
   output += "\n";
-  output += `| Day | Part 1 | Part 2 |\n`;
-  output += `| --- | ------ | ------ |\n`;
+
+  const statRows = [];
 
   for (let i = 0; i < days.length; i++) {
     const day = days[i];
     const dayNumber = i + 1;
     const dayName = `day${dayNumber.toString().padStart(2, "0")}`;
     console.log(`Solving ${dayName}...`);
-    let runner = null;
+    const input = await getInput({ name: dayName, dayNumber });
+    const dayConfig = {
+      ...day,
+      name: dayName,
+      number: dayNumber,
+      input,
+    };
 
+    let runner = null;
     try {
       runner = require(`./${dayName}/index.js`);
     } catch (e) {
       runner = new Runner.Node();
     }
 
-    const solutions = await runner.solve({
-      ...day,
-      name: dayName,
-      number: dayNumber,
-    });
-    output += `| ${dayNumber} `;
-
-    if (solutions.part1.answer === day.part1.answer) {
-      output += `| ★ `;
-    } else {
-      console.log(`Got ${dayNumber} part 1 answer: ${solutions.part1.answer}`);
-      output += `| ☆ `;
+    const templates = runner.getTemplates(dayConfig);
+    for (let template of templates) {
+      if (!fs.existsSync(template.path)) {
+        fs.writeFileSync(template.path, template.content, "utf8");
+      }
     }
 
-    if (solutions.part2.answer === day.part2.answer) {
-      output += `| ★  |\n`;
-    } else {
-      console.log(`Got ${dayNumber} part 2 answer: ${solutions.part2.answer}`);
-      output += `| ☆  |\n`;
+    const solutions = await runner.solve(dayConfig);
+    statRows.push([
+      dayName,
+      solutions.part1.answer === day.part1.answer ? "★ " : "☆ ",
+      solutions.part2.answer === day.part2.answer ? "★ " : "☆ ",
+    ]);
+  }
+
+  output += table([["Day", "Part 1", "Part 2"], ...statRows]);
+
+  fs.writeFileSync(`./README.md`, output, "utf8");
+}
+
+async function getInput({ name, dayNumber }) {
+  const inputPath = `${__dirname}/${name}/input.txt`;
+
+  if (!fs.existsSync(inputPath)) {
+    const url = `https://adventofcode.com/2024/day/${dayNumber}/input`;
+    const newInput = await fetch(url, {
+      headers: {
+        cookie: sessionCookie,
+      },
+    }).then((response) => response.text());
+    fs.writeFileSync(inputPath, newInput, "utf8");
+    return newInput;
+  }
+
+  return fs.readFileSync(inputPath, "utf8");
+}
+
+function table(data) {
+  const [headingRow] = data;
+  let output = "";
+  let columnWidths = headingRow.map(() => 0);
+
+  for (let row of data) {
+    for (let x = 0; x < row.length; x++) {
+      const cell = row[x];
+      if (cell.length > columnWidths[x]) {
+        columnWidths[x] = cell.length;
+      }
     }
   }
 
-  fs.writeFileSync(`./README.md`, output, "utf8");
+  for (let y = 0; y < data.length; y++) {
+    const isHeading = y === 0;
+    const cells = data[y];
+
+    for (let x = 0; x < cells.length; x++) {
+      const cell = cells[x];
+      const columnWidth = columnWidths[x];
+      output += `| ${cell.padEnd(columnWidth, " ")} `;
+    }
+    output += "|\n";
+
+    if (isHeading) {
+      output += `|${columnWidths
+        .map((width) => ` ${"-".repeat(width)} `)
+        .join("|")}|\n`;
+    }
+  }
+
+  return output;
 }
 
 run();
